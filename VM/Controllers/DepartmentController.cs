61b01f7b4af5330db0e5d9afa659caed17df1c1d
robertlek <lecarobertandrei@gmail.com>
2023-10-04 22:25:55 +0300

    Added "DepartmentController" and implemented its "Index" method.

---
+++ VM/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using VM.Models;
+using VM.Storage.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VM.Controllers;
+
+public class DepartmentController : Controller
+{
+    private readonly Context _context;
+
+    public DepartmentController(Context context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        IEnumerable<Department> departments = _context.Departments;
+        return View(departments);
+    }
+}
