f257d5cdf4062d544ac7e6137244cd2e45b68c3a
robertlek <lecarobertandrei@gmail.com>
2023-10-04 23:02:07 +0300

    Implemented Update and Delete methods of the DepartmentController.

--- VM/Controllers/DepartmentController.cs
+++ VM/Controllers/DepartmentController.cs
@@ -36,9 +36,59 @@ public class DepartmentController : Controller
     }
 
     [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id == 0 || id == null)
+        {
+            return NotFound();
+        }
+
+        Department department = _context.Departments.First(department => department.Id == id);
+        return View(department);
+    }
+
+    [HttpPost]
+    [ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmation(Department department)
+    {
+        _context.Departments.Remove(department);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
     public IActionResult Index()
     {
         IEnumerable<Department> departments = _context.Departments;
         return View(departments);
+    }
+
+    [HttpGet]
+    public IActionResult Update(int? id)
+    {
+        if (id == 0 || id == null)
+        {
+            return NotFound();
+        }
+
+        Department department = _context.Departments.First(department => department.Id == id);
+        return View(department);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Update(Department department)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Update(department);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(department);
     }
 }
