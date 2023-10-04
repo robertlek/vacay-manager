54cfeaaa0ff6a8757506fc67e35524a98da00591
robertlek <lecarobertandrei@gmail.com>
2023-10-04 21:55:52 +0300

    Installed EntityFrameworkCore and implemented "Context" class.

---
+++ VM/Storage/DataAccess/Context.cs
@@ -0,0 +1,13 @@
+﻿using VM.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VM.Storage.DataAccess;
+
+public class Context : DbContext
+{
+	public Context(DbContextOptions<Context> options) : base(options)
+	{
+	}
+
+	public DbSet<Department> Departments { get; set; }
+}
