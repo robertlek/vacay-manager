c623f341fbac9696c842a083c3aa38c3cbbc3a7f
robertlek <lecarobertandrei@gmail.com>
2023-10-05 20:28:08 +0300

    Integrated Identity library to the project.

--- VM/Program.cs
+++ VM/Program.cs
@@ -1,12 +1,16 @@
 using VM.Storage.DataAccess;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Configuration.GetConnectionString("DefaultConnection"),
+    option => { option.MigrationsHistoryTable("Migrations", "ef"); }));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<Context>();
 
 var app = builder.Build();
 
@@ -21,6 +25,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
--- VM/Storage/DataAccess/Context.cs
+++ VM/Storage/DataAccess/Context.cs
@@ -1,13 +1,42 @@
 ﻿using VM.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace VM.Storage.DataAccess;
 
-public class Context : DbContext
+public class Context : IdentityDbContext<IdentityUser>
 {
 	public Context(DbContextOptions<Context> options) : base(options)
 	{
 	}
 
 	public DbSet<Department> Departments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityUser>(entity => {
+            entity.ToTable(name: "Users");
+        });
+        builder.Entity<IdentityRole>(entity => {
+            entity.ToTable(name: "Roles");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity => {
+            entity.ToTable("UserRoles");
+        });
+        builder.Entity<IdentityUserClaim<string>>(entity => {
+            entity.ToTable("UserClaims");
+        });
+        builder.Entity<IdentityUserLogin<string>>(entity => {
+            entity.ToTable("UserLogins");
+        });
+        builder.Entity<IdentityUserToken<string>>(entity => {
+            entity.ToTable("UserTokens");
+        });
+        builder.Entity<IdentityRoleClaim<string>>(entity => {
+            entity.ToTable("RoleClaims");
+        });
+    }
 }
