298934b0f5e48db43951293fb695582c89b156c1
robertlek <lecarobertandrei@gmail.com>
2023-10-06 21:09:05 +0300

    Added a public page where the users can see all the departments and their employees / vacations.

---
+++ VM/Areas/Public/Controllers/VacationController.cs
@@ -0,0 +1,21 @@
+﻿using VM.Storage.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VM.Areas.Public.Controllers;
+
+[Area("Public")]
+public class VacationController : Controller
+{
+    private readonly Context _context;
+
+    public VacationController(Context context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var departments = _context.Departments;
+        return View(departments);
+    }
+}
