629e104abcea7fff4364e970c55450a1dc0c88f0
robertlek <lecarobertandrei@gmail.com>
2023-10-08 13:33:48 +0300

    Implemented "EmployeeController" and its "Index" method to view a list of all employees.

---
+++ VM/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using VM.Environment;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using VM.Storage.Repository.IRepository;
+
+namespace VM.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = Roles.Admin)]
+public class EmployeeController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public EmployeeController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
